48dc899699fbc1692dafb012e510eb68660f4c30
Eugene Aslanov <aslanov.ev@gmail.com>
2016-07-06 15:06:24 +0400

    Remove unneeded imports

--- src/main/java/com/socialweb/web/RegistrationController.java
+++ src/main/java/com/socialweb/web/RegistrationController.java
@@ -5,11 +5,8 @@
  */
 package com.socialweb.web;
 
-import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
-import com.socialweb.domain.UserData;
 import com.socialweb.form.RegistrationForm;
 import com.socialweb.service.UserService;
-import java.sql.SQLException;
 import java.util.Locale;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +16,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
